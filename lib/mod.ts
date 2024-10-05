b64bf9abb9b4d09bd05aaf39e78e6e5726b66cc8
oligami <69239094+oligamiq@users.noreply.github.com>
2024-10-05 18:11:49 +0900

    Update mod.ts

--- lib/mod.ts
+++ lib/mod.ts
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2024 oligamiq
+ *
+ * Licensed under the Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
+ * and the MIT License (see LICENSE-MIT for details).
+ */
+
 export type Msg = {
   msg: string,
   id: string,

bb106fbee97ef1689de470abb0469f48bd82d840
oligami <69239094+oligamiq@users.noreply.github.com>
2024-10-05 18:11:34 +0900

    Update index.ts

--- lib/index.ts
+++ lib/index.ts
@@ -1,2 +1,9 @@
+/*
+ * Copyright (c) 2024 oligamiq
+ *
+ * Licensed under the Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
+ * and the MIT License (see LICENSE-MIT for details).
+ */
+
 export { SharedObject } from "./base";
 export { SharedObjectRef } from "./ref";

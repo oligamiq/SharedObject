fd8040e8be20c171e7b73f3b358c4068e6574d47
oligami <69239094+oligamiq@users.noreply.github.com>
2024-10-05 18:12:03 +0900

    Update ref.ts

--- lib/ref.ts
+++ lib/ref.ts
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2024 oligamiq
+ *
+ * Licensed under the Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
+ * and the MIT License (see LICENSE-MIT for details).
+ */
+
 import type { Msg } from "./mod";
 
 export class SharedObjectRef {

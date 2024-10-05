da74c642ac7fd6b6a3743bc6391a39cf3688be64
oligami <69239094+oligamiq@users.noreply.github.com>
2024-10-05 18:14:36 +0900

    Update mod.ts

--- lib/mod.ts
+++ lib/mod.ts
@@ -1,4 +1,5 @@
 /*
+ * Project: https://github.com/oligamiq/SharedObject
  * Copyright (c) 2024 oligamiq
  *
  * Licensed under the Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
